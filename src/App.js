18cf6c9611bde219174410e45bedd80dc0cb182b
Samba Diaw <sambachimerediaw@gmail.com>
2022-02-04 03:11:56 +0100

    wip

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import Contract from "./artifacts/contracts/Raffle.sol/Raffle.json"
 import './App.css';
 
+const { MerkleTree } = require('merkletreejs');
+const keccak256 = require('keccak256');
+const tokens = require('./tokens.json');
+
+const address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 function App() {
+  const [accounts, setAccounts] = useState([]);
+  const [price, setPrice] = useState();
+  
+  useEffect(() => {
+    requestAccount();
+    getPrice(); 
+  }, []);
+
+  async function requestAccount() {
+    if (typeof window.ethereum !== 'undefined') {
+      let account = window.ethereum.request({ method: 'eth_requestAccounts'});
+      setAccounts(accounts);
+    }
+  }
+
+  async function mint() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(address, Contract.abi, signer);
+
+      let tab = [];
+      tokens.map(token => {
+        tab.push(token.address)
+      })
+      const leaves = tab.map(address => keccak256(address));
+      const tree = new MerkleTree(leaves, keccak256, { sort: true });
+      const leaf = keccak256(accounts[0]);
+      const proof = tree.getHexProof(leaf);
+
+      try {
+        let overrides = {
+          from: accounts[0], 
+          value: price,  
+        }
+        const transaction = await contract.mintNFT(accounts[0], proof, overrides);
+        await transaction.wait();
+      } catch(err) {
+        console.log(err);
+      }
+    }
+  }
+
+  async function getPrice() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(address, Contract.abi, provider);
+      try {
+        const data = await contract.getPrice();
+        setPrice(data);
+      } catch(err) {
+        console.log(err);
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={mint}>MINT ONE NFT</button>
+   </div>
   );
 }
 
